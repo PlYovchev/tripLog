c66394f7c1670d7325200431f9fa8d540d162841
svetoslavpopov <svetoslavpopov93@gmail.com>
2015-06-12 14:00:25 +0300

    ImageGallery (beta) implemented

---
+++ TripLog/TripLog/ViewControllers/ImageGalleryViewController.h
@@ -0,0 +1,17 @@
+//
+//  ImageGalleryViewController.h
+//  TripLog
+//
+//  Created by svetoslavpopov on 6/12/15.
+//  Copyright (c) 2015 triOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "iCarousel.h"
+
+
+@interface ImageGalleryViewController : UIViewController <iCarouselDataSource, iCarouselDelegate>
+
+@property (strong, nonatomic) IBOutlet iCarousel *carousel;
+
+@end
